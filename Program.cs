88f307b155dfe2aef62085848e7d7e5de5c76b29
Anastasiya Avdeeva <freesoul89@mail.ru>
2022-10-02 15:23:17 +0300

    create massiv

--- Program.cs
+++ Program.cs
@@ -2,4 +2,15 @@
  Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
 
+Console.WriteLine("Введите любые 5 слов через Enter:");
+string[] stringArray = new string[5];
+string[] stringAr = new string[5];
+    for (int i = 0; i < stringArray.Length; i++){
+        stringArray[i] = Console.ReadLine();
+    }
+Console.WriteLine("Исходный массив:");{
+    for(int i = 0; i < stringArray.Length; i++)
+        Console.Write(" "+ stringArray[i]);
+    }
+  
 
